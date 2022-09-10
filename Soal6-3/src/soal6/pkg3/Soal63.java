b2a6bee56dac351e2bfeef44a81f35130f05f753
Fallure <mfatur757@gmail.com>
2022-09-10 21:02:33 +0700

    Second Commit

--- Soal6-3/src/soal6/pkg3/Soal63.java
+++ Soal6-3/src/soal6/pkg3/Soal63.java
@@ -1,6 +1,19 @@
 package soal6.pkg3;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Soal63 {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        BigInteger var1 = new BigInteger("0");
+        BigInteger var2 = new BigInteger("0");
+        
+        var1 = s.nextBigInteger();
+        var2 = s.nextBigInteger();
+        
+        System.out.println(var1.add(var2));
+        System.out.println(var1.multiply(var2));
     }
-    
 }
